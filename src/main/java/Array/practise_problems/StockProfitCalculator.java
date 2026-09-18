da35d0397cd41b9b07a6eab2e625c8cf2caf0fb9
aakshaya302007 <aakshayavengatesan@gmail.com>
2026-09-18 22:33:53 +0530

    Week_4_Array problems

---
+++ src/main/java/Array/practise_problems/StockProfitCalculator.java
@@ -0,0 +1,44 @@
+package main.java.Array.practise_problems;
+
+import java.util.*;
+
+public class StockProfitCalculator {
+
+    static int maxProfit(int[] prices) {
+
+        int minPrice = prices[0];
+        int maxProfit = 0;
+
+        for (int i = 1; i < prices.length; i++) {
+
+            if (prices[i] < minPrice) {
+                minPrice = prices[i];
+            }
+
+            int profit = prices[i] - minPrice;
+
+            if (profit > maxProfit) {
+                maxProfit = profit;
+            }
+        }
+
+        return maxProfit;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter array size: ");
+        int n = sc.nextInt();
+
+        int[] prices = new int[n];
+
+        System.out.println("Enter stock prices:");
+        for (int i = 0; i < n; i++) {
+            prices[i] = sc.nextInt();
+        }
+
+        System.out.println(maxProfit(prices));
+        sc.close();
+    }
+}
